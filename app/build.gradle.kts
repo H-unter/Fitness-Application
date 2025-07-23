d73edeb5c088f1d985fec3e0b485b404e0ab5b1c
Hunter Kruger-Ilingworth <hkrugerilingworth@gmail.com>
2025-07-23 17:19:58 +1000

    Update build.gradle.kts to add android health connect and increase minimum SDK version to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.fitnessapp"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -70,5 +70,6 @@ dependencies {
 //    implementation("androidx.compose.material3:material3:1.13.0-alpha14")
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
+    implementation("androidx.health.connect:connect-client:1.1.0-alpha10")
     ksp(libs.room.compiler)
 }
